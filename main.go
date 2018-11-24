ab85ff813b1a286bdc12bb9402accb26a827bc3d
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2018-11-24 23:25:43 +0900

    Finish 1st assignment of sec6

--- main.go
+++ main.go
@@ -148,6 +148,42 @@ package main
 // ---------------------------------------------
 // Interfaces with HTTP requests Assignments
 // ---------------------------------------------
+// type triangle struct {
+// 	height float64
+// 	base   float64
+// }
+// type square struct {
+// 	sideLength float64
+// }
+
+// type shape interface {
+// 	getArea() float64
+// }
+
+// func main() {
+// 	t := triangle{
+// 		height: 20.2,
+// 		base:   3.6,
+// 	}
+// 	s := square{
+// 		sideLength: 8.4,
+// 	}
+// 	printArea(t)
+// 	printArea(s)
+// }
+
+// func (t triangle) getArea() float64 {
+// 	return t.height * t.base / 2
+// }
+
+// func (s square) getArea() float64 {
+// 	return s.sideLength * s.sideLength
+// }
+
+// func printArea(s shape) {
+// 	fmt.Println(s.getArea())
+// }
+
 func main() {
 
 }
